555b62003a924156d84537ad1765c5c7d943f1d8
Natalia Kuzmenko <allwdesign@gmail.com>
2023-04-11 18:14:33 +0300

    Add type field to the Toy class

--- Constructor.java
+++ Constructor.java
@@ -1,15 +1,15 @@
 package toys_game;
 
-public class Constructor extends Toy{
-    
+public class Constructor extends Toy {
+
     private int numberOfDetails;
     private String complexity;
-    
-    public Constructor(String name, int quantity, float toyDropRate, int numberOfDetails, String complexity) {
-        super(name, quantity, toyDropRate);
+
+    public Constructor(String type, String name, int quantity, float toyDropRate, int numberOfDetails,
+            String complexity) {
+        super(type, name, quantity, toyDropRate);
         this.numberOfDetails = numberOfDetails;
         this.complexity = complexity;
     }
 
-    
 }
